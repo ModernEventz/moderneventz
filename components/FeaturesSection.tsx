468d84a2afcea8da176b4e2b76613ecc39e05ba4
ModernEventz <eventzmodern@gmail.com>
2024-11-26 09:59:06 +0000

    Update FeaturesSection.tsx

--- components/FeaturesSection.tsx
+++ components/FeaturesSection.tsx
@@ -19,7 +19,7 @@ import {
 import UserCard from "./cards/UserCard";
 import CarouselCards from "./carouselCards";
 async function getData({searchParams}: {searchParams?: { filter?: string}}) {
-  const data = await getVendorsByCategoryNoPagination({ category: searchParams?.filter }:any)
+  const data = await getVendorsByCategoryNoPagination({ category: any })
   return data;
 }
 
