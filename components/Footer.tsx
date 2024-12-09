b8df0755bdff3361491052ec2d595fd48ae5101f
ModernEventz <eventzmodern@gmail.com>
2024-12-09 15:54:52 +0000

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="mb-4 lg:mb-0">
           <p className="text-lg font-semibold">Modern Eventz</p>
           <p className="text-sm">osborn juncion, Kumasi, Ghana</p>
-          <p className="text-sm">info@moderneventz.com</p>
+          <p className="text-sm">info@moderneventsgh.com</p>
         </div>
         <div className="mb-4 lg:mb-0">
           <p className="text-lg font-semibold">Quick Links</p>
