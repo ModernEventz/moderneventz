92b237bc12644997f2efeeb587b9b3ca68bdab4d
ModernEventz <eventzmodern@gmail.com>
2025-01-18 20:38:48 +0000

    Update page.tsx

--- app/(root)/(vendors)/decorListing/page.tsx
+++ app/(root)/(vendors)/decorListing/page.tsx
@@ -80,7 +80,7 @@ import Spinner from '@/components/spinner';
           </div>
           <h3 className="text-sm text-gray-500">{vendor.vendor_name}</h3>
           <div className="mt-1">
-            <span className="font-bold">${vendor.price}</span> per night
+            <span className="font-bold">${vendor.price}</span>
           </div>
         </Link>
     
